7cae4c7923218e836456ac2073cbca0d3b6a581c
Sam Johnson & Shaban Karumba <shaban.karumba@fundingcircle.com>
2014-04-09 12:15:41 +0100

    A user is now able add an ingredient to a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
   end
 
   def show
@@ -10,6 +11,7 @@ class RecipesController < ApplicationController
   
   def create
    @recipe = Recipe.new(recipe_params)
+   # @recipe.ingredients.build
     if @recipe.valid?
       @recipe.save
       redirect_to @recipe
@@ -21,6 +23,6 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:recipe_name, :author, :cooking_time, :serves, :difficulty, ingredients_attributes: [:ingredient_name, :unit, :quantity])
+    params.require(:recipe).permit(:recipe_name, :author, :cooking_time, :serves, :difficulty, ingredients_attributes: [ :ingredient_name, :unit, :quantity ])
   end
 end
